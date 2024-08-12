c55911bc9070a7a84c5f4fddff63f9cb3f4638d6
Marcos Reckers <marcosqlkr@hotmail.com>
2024-08-12 16:24:30 -0300

    make home

--- app.py
+++ app.py
@@ -3,20 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/home')
 def home():
     return render_template('home.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/avaliacao_professores')
-def avaliacao_professores():
-    return render_template('avaliacao_professores.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
