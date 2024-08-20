224389ef37e73fc4a2de1bc54db4f9cc1fc128c5
Marcos Reckers <marcosqlkr@hotmail.com>
2024-08-19 21:19:09 -0300

    pages basic

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+@app.route('/avaliacao_professor')
+def avaliacao_professor():
+    return render_template('avaliacao_professor.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
